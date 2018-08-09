f0fa6ab0c12cc9cb922f2d36a2e818498b13453e
am-mgr <am.mgr.dev@gmail.com>
2018-08-09 23:47:45 +0530

    Added tests

---
+++ main_test.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"blockchain/blockchain"
+	"testing"
+)
+
+func TestVerifyWhenValid(t *testing.T) {
+	bchain := blockchain.NewChain()
+
+	block1 := bchain.NewBlock([]byte("Hello BlockChain"))
+	block2 := bchain.NewBlock([]byte("Greetings"))
+
+	bchain.AddBlock(block1)
+	bchain.AddBlock(block2)
+
+	if !bchain.VerifyChain() {
+		t.Error("Blockchain invalid without change in blocks")
+	}
+}
+
+func TestVerifyWhenInValid(t *testing.T) {
+	bchain := blockchain.NewChain()
+
+	block1 := bchain.NewBlock([]byte("Hello BlockChain"))
+	block2 := bchain.NewBlock([]byte("Greetings"))
+
+	bchain.AddBlock(block1)
+	bchain.AddBlock(block2)
+
+	block2.Content = []byte("Content changed")
+
+	if bchain.VerifyChain() {
+		t.Error("Blockchain valid with change in blocks")
+	}
+}
